25f5d88b949dacd96c7b86a650ed0fc999b715c1
helpimnotdrowning <35247379+helpimnotdrowning@users.noreply.github.com>
2021-02-12 22:27:44 -0600

    Add docstrings, completed REPL

--- ezmath.py
+++ ezmath.py
@@ -1,6 +1,7 @@
+# import all math classes, this is a calculator after all
 from math import *
-import traceback
-import inspect
+# REPL for interactive calculator
+from code import InteractiveConsole
 
 # short helper functions to simplify typing on the REPL
 
@@ -12,7 +13,7 @@ def rad(x):
     '''short for radians()'''
     return radians(x)
     
-# sin, cos and tan n degrees
+# sin, cos and tan in degrees
 # this is because the python.math module works in radians EXCLUSIVELY
 # add degree mode pls
 
@@ -43,10 +44,17 @@ def datan(x):
     '''atan(), but degrees (input and output)'''
     return deg(atan(x))
     
+# simple 2d shapes classes
+
 class shape:
+    '''Does nothing, mainly just here for structure and making inheritence look nice'''
     pass
 
 class circle(shape):
+    '''
+    Circle class to simplify working with circles.
+    Params: radius
+    '''
     def __init__(self, radius):
         self.radius = radius
         self.diameter = radius * 2
@@ -55,48 +63,36 @@ class circle(shape):
         return pi * (self.radius ** 2)
         
 class rectangle(shape):
+    '''
+    Rectangle class to simplify working with rectangles.
+    Params: length and width
+    '''
     def __init__(self, length, width):
         self.length = length
         self.width = width
         
     def area(self):
         return length * width
-
+        
 class square(rectangle):
+    '''
+    Square class to simplify working with square.
+    Params: length
+    '''
     def __init__(self, length):
         super().__init__(length, length)
-
+        
 def cone_volume(radius, height):
     return (1/3) * pi * (radius**2) * height
 
 def cylinder_volume(radius, height):
     return pi * (radius**2) * height
 
-def Convert(tup, di): 
-    for a, b in tup: 
-        di.setdefault(a, []).append(b) 
-    return di 
-      
-# Driver Code     
-tups = ''
-dictionary = {} 
-print (Convert(tups, dictionary)) 
-
-
-from code import InteractiveConsole
-
 if __name__ == '__main__':
-    print('helo welcum to calculator with added stuff :)')
-    print('this prints the result of anything you put in, including the None returned by print()')
-
-    while True:
-        # header = "Welcome to REPL! We hope you enjoy your stay!"
-        # footer = "Thanks for visiting the REPL today!"
-        # scope_vars = {"answer": 42}
-        # InteractiveConsole(locals=scope_vars).interact(header, footer)
-        
-        eval_math = input('> ')
-        try:
-            print(exec(eval_math))
-        except Exception as e: 
-            traceback.print_exc()
+    scope_vars = globals()
+    
+    # these replacements to match what the default python REPL does
+    scope_vars[__name__] = '__console__'
+    scope_vars[__doc__] = None
+    
+    InteractiveConsole(locals=scope_vars).interact('helo welcum to calculator with added stuff :)')
