46c320d030e60a5f2a14d904883199a8a96a1b99
helpimnotdrowning <35247379+helpimnotdrowning@users.noreply.github.com>
2021-05-28 22:47:39 -0500

    Files -> base64, minor changes

--- screencap_helper.py
+++ screencap_helper.py
@@ -1,11 +1,28 @@
 import re
-import pyautogui as phk 
+import pyautogui as phk
 
+from io import BytesIO
 from numpy import asarray
+from base64 import b64decode
+from PIL.Image import open
 from pyperclip import copy as to_clipboard
 from pytesseract import image_to_string
 
 
+# store images as base64 for ease of transport
+# if you decide to run this on your computer, you will probably need to remake these,
+# but idk how MPC-HC handles different resolutions and resizing stuff
+find1 = b'iVBORw0KGgoAAAANSUhEUgAAABQAAAAJBAMAAADA7xF7AAAAKlBMVEUEBAT//wDAwMBCQkIcHBwWFhbMzMzX19eysrL///8zMzPx8fGAgACGhobhABxjAAAARElEQVR4nGNgEGAyZmBgUHEMBZJs5QwM5S0ODCDmTK1FuyFMjnKtRWBRGQGOu1qL7rY4HgSJgpkQBUjMDgaGDjATbi4ADyYV53Bwg2wAAAAASUVORK5CYII='
+find2 = b'iVBORw0KGgoAAAANSUhEUgAAABQAAAAJBAMAAADA7xF7AAAAHlBMVEUAAAAEBAS/v7++vr59fQD+/v77+wDAwMCFhYX4+ADa1Qx6AAAAMklEQVR4nGNgAAFBIUMRAQYIU9UwAcosDzUUhDEjEMwOw5QJcCaSKIwpqGGYCGOCzAUAw1gJqN+0gBIAAAAASUVORK5CYII='
+
+
+# base64 bytes to PIL Image
+def b64_2_PIL_Image(b64):
+    im_bytes = b64decode(b64)
+    im_file = BytesIO(im_bytes)
+    return open(im_file)
+    
+    
 # fixes int strings like '003' -> '3' so python doesnt SyntaxError me
 def fix_int(string):
     return str(int(string))
@@ -36,10 +53,9 @@ def fix_time(time):
     
     # if the regex fails, it return the string unchanged, so this comparison is perfectly valid
     if stripped_time == time:
+        print(f'\n\n{time}\n\n')
         raise RuntimeError('Time could not be stripped.')
         
-    check_time = re.fullmatch('(\d\d:){1,2}\d\d\.\d\d\d', stripped_time)
-    
     # if the video is longer than an hour, MPC-HC will add a XX: to the start of the video elapsed time
     # it does not remove a XX: if the video is shorter than a minute, however.
     if re.fullmatch('(\d\d:){1}\d\d\.\d\d\d', stripped_time):
@@ -49,6 +65,7 @@ def fix_time(time):
         fixed_time = ','.join([fix_int(stripped_time[0:2]), fix_int(stripped_time[3:5]), fix_int(stripped_time[6:8]), fix_int(stripped_time[9:12])])
         
     else:
+        print(f'\n\n{stripped_time}\n\n')
         raise RuntimeError('Time could not be fixed.')
         
     # paste directly into tweet_screencap.py
