d6405a7bc45d9713b93f7d4f1aef048dec035a33
helpimnotdrowning <35247379+helpimnotdrowning@users.noreply.github.com>
2021-07-28 00:46:32 -0500

    almost complete automation of copy+pasting
    pressing SHIFT will copy time; alt tab to editor; paste; alt tab back; and wait for you to press shift again. its beautiful

--- screencap_helper.py
+++ screencap_helper.py
@@ -1,17 +1,18 @@
+# WARNING: This helper was made for a 1680x1050 monitor. If you need to use it on a monitor with a different resolution, make an issue at https://github.com/helpimnotdrowning/tweet_screencap/issues
+
 import re
 import pyautogui as phk
 
 from io import BytesIO
+from time import sleep
 from numpy import asarray
 from base64 import b64decode
 from PIL.Image import open
 from pyperclip import copy as to_clipboard
 from pytesseract import image_to_string
-
+from pynput.keyboard import Key, Listener, Controller
 
 # store images as base64 for ease of transport
-# if you decide to run this on your computer, you will probably need to remake these,
-# but idk how MPC-HC handles different resolutions and resizing stuff
 find1 = b'iVBORw0KGgoAAAANSUhEUgAAABQAAAAJBAMAAADA7xF7AAAAKlBMVEUEBAT//wDAwMBCQkIcHBwWFhbMzMzX19eysrL///8zMzPx8fGAgACGhobhABxjAAAARElEQVR4nGNgEGAyZmBgUHEMBZJs5QwM5S0ODCDmTK1FuyFMjnKtRWBRGQGOu1qL7rY4HgSJgpkQBUjMDgaGDjATbi4ADyYV53Bwg2wAAAAASUVORK5CYII='
 find2 = b'iVBORw0KGgoAAAANSUhEUgAAABQAAAAJBAMAAADA7xF7AAAAHlBMVEUAAAAEBAS/v7++vr59fQD+/v77+wDAwMCFhYX4+ADa1Qx6AAAAMklEQVR4nGNgAAFBIUMRAQYIU9UwAcosDzUUhDEjEMwOw5QJcCaSKIwpqGGYCGOCzAUAw1gJqN+0gBIAAAAASUVORK5CYII='
 
@@ -30,19 +31,10 @@ def fix_int(string):
     
 # finds MPC-HC timestamp in screen
 def find_time():
-    screenshot = phk.screenshot()
+    # Instead of searching the whole screen, search only in the middle-bottom-left (in that order). gives a bit of speedup
+    screenshot = phk.screenshot(region=(666,984,145,22))
     
-    fimg_coords = phk.locate(b64_2_PIL_Image(find1), screenshot)
-    
-    if not fimg_coords:
-        fimg_coords = phk.locate(b64_2_PIL_Image(find2), screenshot)
-        
-    if not fimg_coords:
-        raise RuntimeError('Couldn\'t locate find.png on-screen. Make sure MPC-HC is open and the timestamp is visible.')
-        
-    time_img = screenshot.crop((fimg_coords[0]-175,fimg_coords[1]-6,fimg_coords[0],fimg_coords[1] + fimg_coords[3]+6))
-    
-    return image_to_string(asarray(time_img), config="-c tessedit_char_whitelist=0123456789:.\/")
+    return image_to_string(asarray(screenshot), config="-c tessedit_char_whitelist=0123456789:.\/")
     
     
 # fixes up timestamp to be directly pasted into tweet_screencap.py
@@ -70,15 +62,41 @@ def fix_time(time):
         
     # paste directly into tweet_screencap.py
     return f'''sec = time_to_seconds({fixed_time})
-                elif sec == time_to_seconds({fixed_time}):
-                    '''
+                elif sec == time_to_seconds({fixed_time}):  '''
     
     
-if __name__ == '__main__':
-    while True:
-        w = input('h?')
+def on_press(key):
+    pass
+        
+        
+# i am very unreasonably proud of this
+# this finds and copies the time to the clipboard,
+# alt-tabs to last app (should be n++ or whatever you may be editing tweet_screencap.py with
+# pastes,
+# and then alt-tabs back
+def on_release(key):
+    if key == Key.shift:   
         found_time = find_time()
         print(found_time)
         time = fix_time(found_time)
         to_clipboard(time)
+        
+        with Controller().pressed(Key.alt):
+            Controller().tap(Key.tab)
+            
+        sleep(.5) # pause for a bit because if not, then it will send the paste to the task switcher and not notepad++
+        Controller().press(Key.ctrl.value)
+        Controller().tap('v')
+        Controller().release(Key.ctrl.value)
+       
+        with Controller().pressed(Key.alt):
+            Controller().tap(Key.tab)
+            
+if __name__ == '__main__':
     
+    print("ello & ready, press SHIFT to go")
+    
+    # Collect events until released
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+        
